35e71c8eb6c9027b20418361ac519e8244ada923
Leonid Mihaylov <leonmixt@gmail.com>
2023-02-26 23:04:19 +0400

    Edited

--- src/test/java/e2e/ApplicationManager.java
+++ src/test/java/e2e/ApplicationManager.java
@@ -5,7 +5,7 @@ import e2e.helpers.CreateContactHelpers;
 import e2e.helpers.EditContactHelpers;
 import e2e.helpers.LoginHelpers;
 import e2e.helpers.RegisterHelpers;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -40,7 +40,8 @@ public class ApplicationManager {
     }
 
     protected void init() {
-        WebDriverManager.chromedriver().setup();
+
+        ChromeDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("http://phonebook.telran-edu.de:8080/");
         driver.manage().window().maximize();
