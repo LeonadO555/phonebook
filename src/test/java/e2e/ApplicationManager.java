9c6134aec78572775c380c94ef24c9c6fe164a83
Leonid Mihaylov <leonmixt@gmail.com>
2023-02-28 00:39:27 +0400

    Edited

--- src/test/java/e2e/ApplicationManager.java
+++ src/test/java/e2e/ApplicationManager.java
@@ -5,15 +5,15 @@ import e2e.helpers.CreateContactHelpers;
 import e2e.helpers.EditContactHelpers;
 import e2e.helpers.LoginHelpers;
 import e2e.helpers.RegisterHelpers;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -41,21 +41,23 @@ public class ApplicationManager {
     }
 
     protected void init() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        //***********Chrome*********** */
-        //capabilities.setBrowserName("chrome");
-        //capabilities.setVersion("76.0");
-        //***********FireFox***********/
-        capabilities.setBrowserName("firefox");
-        capabilities.setVersion("90.0");
-        //***********Opera*********** */
-//        capabilities.setBrowserName("chrome");
-//        capabilities.setVersion("62.0");
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableLog", true);
-        driver = new RemoteWebDriver(
-                URI.create("http://127.0.0.1:4444/wd/hub").toURL(),
-                capabilities);
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        //***********Chrome*********** */
+//        //capabilities.setBrowserName("chrome");
+//        //capabilities.setVersion("76.0");
+//        //***********FireFox***********/
+//        capabilities.setBrowserName("firefox");
+//        capabilities.setVersion("90.0");
+//        //***********Opera*********** */
+////        capabilities.setBrowserName("chrome");
+////        capabilities.setVersion("62.0");
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableLog", true);
+//        driver = new RemoteWebDriver(
+//                URI.create("http://127.0.0.1:4444/wd/hub").toURL(),
+//                capabilities);
         driver.get("http://phonebook.telran-edu.de:8080/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
