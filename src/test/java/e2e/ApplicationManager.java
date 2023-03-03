522dbee6c6435a570e592cdbdb8e90c52c2798c5
Leonid Mihaylov <leonmixt@gmail.com>
2023-03-03 13:46:23 +0400

    Added method for remote driver

--- src/test/java/e2e/ApplicationManager.java
+++ src/test/java/e2e/ApplicationManager.java
@@ -5,15 +5,16 @@ import e2e.helpers.CreateContactHelpers;
 import e2e.helpers.EditContactHelpers;
 import e2e.helpers.LoginHelpers;
 import e2e.helpers.RegisterHelpers;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -22,7 +23,7 @@ public class ApplicationManager {
     LoginHelpers login;
     RegisterHelpers register;
     CreateContactHelpers createContact;
-    EditContactHelpers editeContact;
+    EditContactHelpers editContact;
 
     public LoginHelpers getLogin() {
         return login;
@@ -37,20 +38,26 @@ public class ApplicationManager {
     }
 
     public EditContactHelpers getEditContact() {
-        return editeContact;
+        return editContact;
+    }
+
+    public WebDriver remoteDriverSelenoid() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox");
+        capabilities.setVersion("90.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableLog", true);
+        driver = new RemoteWebDriver(
+                URI.create("http://127.0.0.1:4444/wd/hub").toURL(),
+                capabilities);
+        return driver;
     }
 
     protected void init() throws MalformedURLException {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setBrowserName("firefox");
-//        capabilities.setVersion("90.0");
-//        capabilities.setCapability("enableVNC", true);
-//        capabilities.setCapability("enableLog", true);
-//        driver = new RemoteWebDriver(
-//                URI.create("http://127.0.0.1:4444/wd/hub").toURL(),
-//                capabilities);
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+        driver = remoteDriverSelenoid();
+
         driver.get("http://phonebook.telran-edu.de:8080/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -58,7 +65,7 @@ public class ApplicationManager {
         login = new LoginHelpers(driver);
         register = new RegisterHelpers(driver);
         createContact = new CreateContactHelpers(driver);
-        editeContact = new EditContactHelpers(driver);
+        editContact = new EditContactHelpers(driver);
     }
 
     public String takeScreenshot() throws IOException {
