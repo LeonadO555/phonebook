28c3926e68c99e83ab8ca50f07de3f9691e4633c
coss <cossx@yahoo.com>
2023-03-07 17:03:25 +0600

    07_03_2023

---
+++ src/test/java/ui/page/MainMenu.java
@@ -0,0 +1,25 @@
+package ui.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MainMenu extends PageBase{
+    WebDriver driver;
+
+    public MainMenu(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//ul[@class='navbar-nav mr-auto']/li[2]")
+    private WebElement addNewContactMenuLink;
+
+    public boolean isAddNewContactMenuLinkDisplayed() {
+        return addNewContactMenuLink.isDisplayed();
+    }
+
+    public void clickToAddNewContactMenuLink() {
+        addNewContactMenuLink.click();
+    }
+}
---
+++ src/test/java/ui/page/PageBase.java
@@ -0,0 +1,13 @@
+package ui.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class PageBase {
+    protected WebDriver driver;
+
+    public PageBase(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
